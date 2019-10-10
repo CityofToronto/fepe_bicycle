b1b9d4530db82b3925a0eecbd381f72d85c734c4
James Ngo <James.Ngo@toronto.ca>
2019-10-10 11:59:11 -0400

    Move Style to Style

--- src/scripts/views/thankyou_page.js
+++ src/scripts/views/thankyou_page.js
@@ -2,6 +2,8 @@
 
 /* exported ThankYouPageView */
 const ThankYouPageView = BaseView.extend({
+  className: 'thankYouPage',
+
   events: {
     ['click .btn-done'](event) {
       event.preventDefault();
@@ -19,18 +21,19 @@ const ThankYouPageView = BaseView.extend({
 
           <table class="table table-bordered">
             <tbody>
-              <tr><th colspan="2" style="background-color: #eeeeee;">Contact Information</th></tr>
+              <tr><th colspan="2">Contact Information</th></tr>
               <tr><td width="33%">Confirmation ID</td><td>${this.model.escape('id')}</td></tr>
               <tr><td width="33%">First Name</td><td>${this.model.escape('first_name')}</td></tr>
               <tr><td>Last Name</td><td>${this.model.escape('last_name')}</td></tr>
               <tr><td>Title</td><td>${this.model.escape('title')}</td></tr>
+              <tr><td>Primary Phone Number</td><td>${this.model.escape('email')}</td></tr>
               <tr><td>Primary Phone Number</td><td>${this.model.escape('primary_phone')}</td></tr>
               <tr><td>Alternate Phone Number</td><td>${this.model.escape('alternate_phone')}</td></tr>
               <tr><td>Address</td><td>${this.model.escape('street_address')}</td></tr>
               <tr><td>City</td><td>${this.model.escape('city')}</td></tr>
               <tr><td>Province</td><td>${this.model.escape('province')}</td></tr>
               <tr><td>Postal Code</td><td>${this.model.escape('postal_code')}</td></tr>
-              <tr><th colspan="2" style="background-color: #eeeeee;">Request Information</th></tr>
+              <tr><th colspan="2">Request Information</th></tr>
               <tr><td>Type</td><td>${this.model.escape('type')}</td></tr>
               ${
                 this.model.get('type') === 'Bicycle Lockers'
@@ -43,7 +46,7 @@ const ThankYouPageView = BaseView.extend({
                   <tr><td>Choice 1</td><td>${this.model.escape('stations_choice_1')}</td></tr>
                   <tr><td>Choice 2</td><td>${this.model.escape('stations_choice_2') || '-'}</td></tr>
                   <tr><td>Choice 3</td><td>${this.model.escape('stations_choice_3') || '-'}</td></tr>
-                  <tr><th colspan="2" style="background-color: #eeeeee;">Bicycle Information</th></tr>
+                  <tr><th colspan="2">Bicycle Information</th></tr>
                   <tr><td>Bicycle 1</td><td>${[
                     this.model.get('bicycle_1_make') ? `Make: ${this.model.escape('bicycle_1_make')}` : false,
                     this.model.get('bicycle_1_model') ? `Model: ${this.model.escape('bicycle_1_model')}` : false,
