4e1d2ecbca3ff2b47c4812ad109795013671d5ab
James Ngo <James.Ngo@toronto.ca>
2019-10-10 11:57:24 -0400

    Fix QA URL

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ core.embeddedApp.createTasks(gulp, {
     },
     qa: {
       RECAPTCHA_SITEKEY: '6LeN_XIUAAAAAEd8X21vFtkJ3_c7uA0xpUGcrGpe',
-      RECAPTCHA_API_URL: 'https://was-intra-sit.toronto.ca/c3api_data/v2/DataAccess.svc/cot_dts_recaptcha/app_config/ca.toronto.api.dataaccess.odata4.verify',
+      RECAPTCHA_API_URL: 'https://was-intra-qa.toronto.ca/c3api_data/v2/DataAccess.svc/cot_dts_recaptcha/app_config/ca.toronto.api.dataaccess.odata4.verify',
 
       RECAPTCHA_REGISTRATION_CONFIG_TOKEN: '387825d4-cdc2-4c50-9029-516a0eeb28f5',
 
